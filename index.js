e409cd67e5e85eedcdc34d35d51bce881f3ec8e6
Dornavan <130479724+dorndickence@users.noreply.github.com>
2023-10-15 11:39:34 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const { create } = require('@open-wa/wa-automate')
-const msgHandler = require('./msgHandler')
-const welcome = require('./lib/welcome.js')
-const fs = require('fs-extra')
+const { create } = require('@open-wa/wa-automate');
+const msgHandler = require('./msgHandler');
+const welcome = require('./lib/welcome.js');
+const fs = require('fs-extra');
+
 const serverOption = {
     headless: true,
     cacheEnabled: false,
@@ -17,57 +18,59 @@ const serverOption = {
     ]
 }
 
-const opsys = process.platform
+const opsys = process.platform;
+
 if (opsys === 'win32' || opsys === 'win64') {
-    serverOption.executablePath = 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe'
+    serverOption.executablePath = 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe';
 } else if (opsys === 'linux') {
-    serverOption.browserRevision = '737027'
+    serverOption.browserRevision = '737027';
 } else if (opsys === 'darwin') {
-    serverOption.executablePath = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+    serverOption.executablePath = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome';
 }
 
 const startServer = async (client) => {
-        global.sclient = client
-        global.sendingAnimatedSticker = []
-        global.queueAnimatedSticker = []
-        global.amdownloaden = []
-        global.queuemp3 = []
-        global.queuemp4 = []
-        console.log('[SERVER] Server Started!')
-        // Force it to keep the current session
-        client.onStateChanged((state) => {
-                console.log('[Client State]', state)
-                if (state === 'CONFLICT' || state === 'UNLAUNCHED') client.forceRefocus()
-        })
-        // listening on message
-        client.onMessage((message) => {
-            msgHandler(client, message)
-        })
+    global.sclient = client;
+    global.sendingAnimatedSticker = [];
+    global.queueAnimatedSticker = [];
+    global.amDownloaden = [];
+    global.queueMp3 = [];
+    global.queueMp4 = [];
+    console.log('[SERVER] Server Started!');
 
-        client.onGlobalParicipantsChanged((event) => {
-            welcome(client, event)
-        }) 
-        
-        client.onAddedToGroup((chat) => {
-            let totalMem = chat.groupMetadata.participants.length
-            if (totalMem < 3 
-            	client.sendText(chat.id, `Este grupo só tem ${totalMem} membros, são necessários pelo menos 3 membros para ativar os serviços`).then(() => client.leaveGroup(chat.id))
-            	client.deleteChat(chat.id)
-            } else {
-                client.sendText(chat.groupMetadata.id, `Obrigado por me adicionar *${chat.contact.name}*. Use #help para saber meus comandos`)
-            }
-        })
-       
-        // listening on Incoming Call
-        client.onIncomingCall((call) => {
-            client.sendText(call.peerJid, '...')
-            client.contactBlock(call.peerJid)
-            ban.push(call.peerJid)
-            fs.writeFileSync('./lib/banned.json', JSON.stringify(ban))
-        })
-    }
+    // Force it to keep the current session
+    client.onStateChanged((state) => {
+        console.log('[Client State]', state);
+        if (state === 'CONFLICT' || state === 'UNLAUNCHED') client.forceRefocus();
+    });
+
+    // listening on message
+    client.onMessage((message) => {
+        msgHandler(client, message);
+    });
+
+    client.onGlobalParticipantsChanged((event) => {
+        welcome(client, event);
+    });
+
+    client.onAddedToGroup((chat) => {
+        let totalMembers = chat.groupMetadata.participants.length;
+        if (totalMembers < 3) {
+            client.sendText(chat.id, `This group has only ${totalMembers} members, at least 3 members are required to activate the services`).then(() => client.leaveGroup(chat.id));
+            client.deleteChat(chat.id);
+        } else {
+            client.sendText(chat.groupMetadata.id, `Thank you for adding me, *${chat.contact.name}*. Use #help to know my commands.`);
+        }
+    });
+
+    // listening on Incoming Call
+    client.onIncomingCall((call) => {
+        client.sendText(call.peerJid, '...');
+        client.contactBlock(call.peerJid);
+        ban.push(call.peerJid);
+        fs.writeFileSync('./lib/banned.json', JSON.stringify(ban));
+    });
+}
 
 create('session', serverOption)
-    const  botadmins  =  [ '254710710881926@c.ke' , '2547108819026@c.ke' , '254710881926@c.ke' ]
-	.then(async (client) => startServer(client))
-    .catch((error) => console.log(error))
+    .then(async (client) => startServer(client))
+    .catch((error) => console.log(error));
